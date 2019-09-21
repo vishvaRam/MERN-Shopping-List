247dfa1c2cc64fa7493b0e0b21baaa6c1431e4c0
vishvaRam <44122284+vishvaRam@users.noreply.github.com>
2019-09-21 11:28:55 +0100

    server

--- config/key.js
+++ config/key.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURL : "mongodb://vishva:vishva@cluster0-shard-00-00-pjypg.mongodb.net:27017,cluster0-shard-00-01-pjypg.mongodb.net:27017,cluster0-shard-00-02-pjypg.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+    mongoURL : "mongodb+srv://vishva:vishva@mern-stack-shoppinglist-vawd8.mongodb.net/ShoppingListApp?retryWrites=true&w=majority"
 }
